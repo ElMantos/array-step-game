444f97b92900e498dc25639b958f50a76d29de45
Mantas Giniunas <mantasginiunas@Mantass-MacBook-Pro.local>
2020-04-26 15:57:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# array-step-game
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <div>Initial app</div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
